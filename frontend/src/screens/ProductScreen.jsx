c61ad92ca0c49ce56b17f1e25fabe1c0f9027e1e
waqas <wmunir232@gmail.com>
2024-03-09 14:10:25 +0500

    Added Loader and Alert Component

--- frontend/src/screens/ProductScreen.jsx
+++ frontend/src/screens/ProductScreen.jsx
@@ -3,6 +3,8 @@ import { Link, useParams } from "react-router-dom";
 import { Row, Col, Image, ListGroup, Card, Button } from "react-bootstrap";
 
 import Rating from "../components/Rating";
+import Loader from "../components/Loader";
+import Message from "../components/Message";
 
 const ProductScreen = () => {
   const { id } = useParams();
@@ -18,9 +20,9 @@ const ProductScreen = () => {
         Go Back
       </Link>
       {isLoading ? (
-        <h2>Loading...</h2>
+        <Loader />
       ) : error ? (
-        <h2>{error?.data.message || error.error}</h2>
+        <Message variant="danger">{error?.data.message || error.error}</Message>
       ) : (
         <>
           <Row>
